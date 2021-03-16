1e9b4011a047cd2359644a5e7c7e50e0b04a1ec4
arcusmark <mark.denotter@arcusit.nl>
2021-03-16 15:30:35 +0100

    Update

---
+++ Deploy_ADDS.ps1
@@ -0,0 +1,45 @@
+Configuration ADDomain_NewForest_Config
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $domainName    
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $Credential,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        $SafeModePassword
+    )
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName ActiveDirectoryDsc
+
+    node 'localhost'
+    {
+        WindowsFeature 'ADDS'
+        {
+            Name   = 'AD-Domain-Services'
+            Ensure = 'Present'
+        }
+
+        WindowsFeature 'RSAT'
+        {
+            Name   = 'RSAT-AD-PowerShell'
+            Ensure = 'Present'
+        }
+
+        ADDomain $domainName
+        {
+            DomainName                    = $domainName
+            Credential                    = $Credential
+            SafemodeAdministratorPassword = $SafeModePassword
+            ForestMode                    = 'WinThreshold'
+        }
+    }
+}
