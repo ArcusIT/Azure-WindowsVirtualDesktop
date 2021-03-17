4bea79868966d413deec25c7b13868fb247b13f0
arcusmark <mark.denotter@arcusit.nl>
2021-03-17 17:14:53 +0100

    update

--- Install_7zip.ps1
+++ Install_7zip.ps1
@@ -1,4 +1,4 @@
-$Installer7Zip = $env:TEMP + "\7z1900-x64.msi"
-Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile $Installer7Zip
-msiexec /i $Installer7Zip /qb
-Remove-Item $Installer7Zip
+$Installer7Zip = $env:TEMP + "\7z1900-x64.msi";
+Invoke-WebRequest "https://www.7-zip.org/a/7z1900-x64.msi" -OutFile $Installer7Zip;
+msiexec /i $Installer7Zip /qb;
+Remove-Item $Installer7Zip;
