e833ef9eba820d582cba186da335588070b264b6
otterm83 <mark@otterm.nl>
2021-04-22 16:41:44 +0200

    Update

--- install_script.ps1
+++ install_script.ps1
@@ -1,3 +1,8 @@
+#Set-ExecutionPolicy Bypass
+#$Script = Invoke-WebRequest 'https://raw.githubusercontent.com/ArcusIT/Azure-WindowsVirtualDesktop/main/install_script.ps1' -UseBasicParsing
+#$ScriptBlock = [Scriptblock]::Create($Script.Content)
+#Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @('someargument'))
+
 Import-Module PowerShellGet
 
 #region Start logging
